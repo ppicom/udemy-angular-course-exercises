dc30cc6c7fbd8706d848279bd55f97a98c1a93a2
Pere Picó Muntaner <perepicomuntaner@gmail.com>
2021-12-20 19:43:00 +0100

    Complete template-driven approach exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'exercises';
+  levels: { value: string; display: string }[] = [
+    {
+      display: 'Pro',
+      value: 'pro',
+    },
+    {
+      display: 'Advanced',
+      value: 'advanced',
+    },
+    {
+      display: 'Basic',
+      value: 'basic',
+    },
+  ];
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
+  }
 }
